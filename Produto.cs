f3db7bd7ad30959fadb1d880ad5d51c2dc277666
Markus <machelmarkus@gmail.com>
2021-09-13 21:38:26 -0300

    Com properties

--- Produto.cs
+++ Produto.cs
@@ -8,6 +8,26 @@ namespace aula_construtores
         private double _preco;
         private int _quantidade;
 
+         public string Nome 
+        {
+            get { return _nome; }
+            set { if (value != null && value.Length > 1)
+            {
+            _nome = value;
+            } }
+        }
+
+        public double Preco 
+        {
+            get { return _preco;}
+        }
+    
+
+        public int Quantidade
+        {
+            get { return _quantidade;}
+        }
+
         public Produto()
         {}
 
@@ -16,29 +36,6 @@ namespace aula_construtores
             _nome = nome;
             _preco = preco;
             _quantidade = quantidade;
-        }
-
-        public string GetNome() 
-        {
-            return _nome;
-        }
-
-        public void SetNome(string nome)
-        {
-            if (nome != null && nome.Length > 1)
-            {
-            _nome = nome;
-            }
-        }
-
-        public double GetPreco()
-        {
-            return _preco;
-        }
-
-        public int GetQuantidade()
-        {
-            return _quantidade;
         }
 
         public double ValorTotalEmEstoque() 
